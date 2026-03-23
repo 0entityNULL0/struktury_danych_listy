a7ab470d8cf1d22899d8716ed937f1746b7cb179
ptakj <ptakjakub004@gmail.com>
2026-03-23 02:08:29 +0100

    find LL

--- sources/linked_list.h
+++ sources/linked_list.h
@@ -23,10 +23,21 @@ private:
     }
 
     Node* getLast(){
-        if(this->head == nullptr){return nullptr};
+        if(this->head == nullptr){return nullptr;}
 
         Node* temp = this->head;
         while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        return temp;
+    }
+    Node* getOnPossition(long long int pos){
+        if(this->head == nullptr || pos < 0){return nullptr;}
+
+        Node* temp = this->head;
+        for (int i=1; i<=pos; i++)
+        {
+            if(temp->next == nullptr) return nullptr;
             temp = temp->next;
         }
         return temp;
@@ -70,7 +81,24 @@ private:
         }        
     }
 
+    bool isEmpty() //1 empty, 0 no
+    {
+        return this->head == nullptr;
+    }
 
+    long long int len()
+    {
+        if(this->head == nullptr){return 0;}
+        long long int ln=1;
+        Node *tmp = this->head;
+
+        while (tmp->next != nullptr)
+        {
+            tmp = tmp->next;
+            ln++;
+        }     
+        return ln;
+    }
 
     
     LinkedList& operator=(LinkedList<T>&& other)
@@ -110,13 +138,67 @@ private:
     {
         if (this->head == nullptr)
         {
-            thi->head = new Node(val);
+            this->head = new Node(val);
         }
         else
         {
         Node* tmplast = this->getLast();
         tmplast->next = new Node(val);
         }
+    }
 
+    void addFirst (T val)
+    {
+        if (this->head == nullptr)
+        {
+            this->head = new Node(val);
+        }
+        else
+        {
+        Node* tmpfirst = new Node(val);
+        tmpfirst->next = this->head;
+        this->head = tmpfirst;
+        }
+    }
+
+    void addAtPos (T val, long long int pos)
+    {
+        if (this->isEmpty() && pos == 0)
+        {
+            this->head = new Node (val);
+            return;
+        }
+
+        if (pos == 0)
+        {
+            this->addFirst(val);
+            return;
+        }
+
+        if(this->getOnPossition(pos-1) == nullptr) return;
+        else
+        {
+            Node *tmp = this->getOnPossition(pos-1);
+            Node *tmp2 = tmp->next;
+            tmp->next = new Node(val);
+            tmp = tmp->next;
+            tmp-> next = tmp2;
+        }
+    }
+
+    long long int findValue(T val)//returns index, if not -1
+    {
+        long long int ind =0;
+        if(this->head == nullptr ){return -1;}
+
+        Node* temp = this->head;
+        while(temp != nullptr)
+        {
+            if(temp->data == val) return ind;
+            temp = temp->next;
+            ind++;
+        }
+        return -1;
+    }
 };
 
