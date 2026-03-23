c08f9e1c7cde7aaef2f9a42f8fccb55e533a610d
ptakj <ptakjakub004@gmail.com>
2026-03-23 01:06:19 +0100

    getLast, addLast functions

--- sources/linked_list.h
+++ sources/linked_list.h
@@ -8,10 +8,30 @@ private:
         T data;
         Node* next;
         Node(T val) : data(val), next(nullptr){}
-    }
+    };
 
     Node* head;
 
+    void clear() {
+        Node* temp;
+        while (head != nullptr) {
+            temp = head->next;
+            delete head;
+            head = temp;
+        }
+        head = nullptr;
+    }
+
+    Node* getLast(){
+        if(this->head == nullptr){return nullptr};
+
+        Node* temp = this->head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        return temp;
+    }
+    
     public:
     LinkedList() : head(nullptr){};
 
@@ -28,11 +48,11 @@ private:
 
     LinkedList(LinkedList&& other)
     {
-        this->head = copy.head;
-        other->head = nullptr;
+        this->head = other.head;
+        other.head = nullptr;
     }
 
-    LinkedList(LinkedList<T> const &copy)
+    LinkedList(const LinkedList<T> &copy)
     {
         if(copy.head == nullptr){this->head = nullptr; return;}
 
@@ -45,14 +65,58 @@ private:
         {
             tmp->next = new Node(tmp2->data);
 
-            tmp.head = tmp->next;
-            tmp2.head = tmp2->next;
+            tmp = tmp->next;
+            tmp2 = tmp2->next;
         }        
     }
 
 
 
+    
+    LinkedList& operator=(LinkedList<T>&& other)
+    {
+        if (this == &other) return *this;
+        
+        this->clear();
 
+        this->head = other.head;
+        other.head = nullptr;
 
-}
+        return *this;
+    }
+
+    LinkedList& operator=(const LinkedList<T>& copy)
+    {
+        this->clear();
+        if (copy.head == nullptr) return *this;
+
+        this->head = new Node(copy.head->data);
+
+        Node *tmp = this->head;
+        Node *tmp2 = copy.head->next;
+
+        while (tmp2 != nullptr)
+        {
+            tmp->next = new Node(tmp2->data);
+
+            tmp = tmp->next;
+            tmp2 = tmp2->next;
+        }
+
+        return *this;     
+    }
+
+    void addLast (T val)
+    {
+        if (this->head == nullptr)
+        {
+            thi->head = new Node(val);
+        }
+        else
+        {
+        Node* tmplast = this->getLast();
+        tmplast->next = new Node(val);
+        }
+
+};
 
